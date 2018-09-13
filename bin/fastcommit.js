aca4ef16b241fc4d135432655ea6a7240f19863d
Patricio Pitaluga <patricio.pitaluga@avatarla.com>
2018-09-13 17:54:03 -0300

    .

--- bin/fastcommit.js
+++ bin/fastcommit.js
@@ -6,68 +6,66 @@ const branch = require('git-branch');
 const repoName = require('git-repo-name');
 
 let commitmessage = '';
-function step1() {
-  prompt.start();
-
-  prompt.get([
-    {
+async () => {
+  await new Promise((resolve, reject) => {
+    prompt.start();
+    prompt.get([{
       name: 'commitmessage',
       description: 'Commit message (enter for .)'
-    }
-  ], function(err, result) {
-    commitmessage = result.commitmessage;
-    step2();
-  });
-}
-function step2() {
-  exec('git add .', (err, stdout, stderr) => {
-    if (err) throw err;
-    if (commitmessage === '') commitmessage = '.';
-    console.log(stdout);
-    step3();
-  });
-}
-function step3() {
-  exec('git commit -am "' + commitmessage + '"', (err, stdout, stderr) => {
-    if (err) {
-      if (err) {
-        console.log.bind(err);
-        console.log(stdout);
-        return;
-      }
-      step4();
-    } else {
-      step4();
-    }
-  });
-}
-function step4() {
-  branch(function(err, branchName) {
-    exec('git push origin ' + branchName, (err, stdout, stderr) => {
-      if (err) {
-        console.log.bind(err);
-        console.log(stdout);
-        return;
-      }
-      console.log(stdout);
-      //step5();
+    }], function(err, result) {
+      commitmessage = result.commitmessage;
+      resolve();
     });
   });
-}
-/*function step5() {
-  console.log('\x1b[32m', 'Create a merge request from source branch ' + branchName + ' to dev');
-  console.log('\x1b[0m', '');
-
-  repoName(function(err, currentRepoName) {
-    exec('start https://bitbucket.org/ /' + currentRepoName + '/pull-requests/new', (err, stdout, stderr) => {
-    if (err) throw err;
-    console.log(stdout);
-
-    exec('git checkout dev', (err, stdout, stderr) => {
+  await new Promise((resolve, reject) => {
+    exec('git add .', (err, stdout, stderr) => {
       if (err) throw err;
-
-    });
+      if (commitmessage === '') commitmessage = '.';
+      console.log(stdout);
+      resolve();
     });
   });
-}*/
-step1();
+  await new Promise((resolve, reject) => {
+    exec('git commit -am "' + commitmessage + '"', (err, stdout, stderr) => {
+      if (err) {
+        if (err) {
+          console.log.bind(err);
+          console.log(stdout);
+          return;
+        }
+        step4();
+      } else {
+        step4();
+      }
+    });
+  });
+  await new Promise((resolve, reject) => {
+    branch(function(err, branchName) {
+      exec('git push origin ' + branchName, (err, stdout, stderr) => {
+        if (err) {
+          console.log.bind(err);
+          console.log(stdout);
+          return;
+        }
+        console.log(stdout);
+        //step5();
+      });
+    });
+  });
+  await new Promise((resolve, reject) => {
+    console.log('\x1b[32m', 'Create a merge request from source branch ' + branchName + ' to dev');
+    console.log('\x1b[0m', '');
+
+    repoName(function(err, currentRepoName) {
+      exec('start https://bitbucket.org/ /' + currentRepoName + '/pull-requests/new', (err, stdout, stderr) => {
+      if (err) throw err;
+      console.log(stdout);
+
+      exec('git checkout dev', (err, stdout, stderr) => {
+        if (err) throw err;
+
+      });
+      });
+    });
+  });
+}();
--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
+const repoName = require('git-repo-name');
+
 console.log(123);
 process.stdout.write("whuut");
+
+repoName('../', function(err, currentRepoName) {
+  console.log(currentRepoName);
+});
