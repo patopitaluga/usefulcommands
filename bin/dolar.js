ac75a46af2117d190fccc2aea8f1eac5e61f85cc
Patricio Pitaluga <patricio.pitaluga@avatarla.com>
2018-09-14 12:35:37 -0300

    Get dolar value in Arg

---
+++ bin/dolar.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+
+axios.get('https://www.google.com.ar/search?q=1usd+to+ars&oq=1usd+to+ars&aqs=chrome..69i57.5664j0j1&sourceid=chrome&ie=UTF-8')
+  .then(function (response) {
+    let valDolar = response.data.substr(response.data.indexOf('<b>1 USD</b> = ') + 15);
+    valDolar = valDolar.substr(0, valDolar.indexOf(' '));
+    console.log('1usd = $' + valDolar + ' pesos argentinos.');
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
