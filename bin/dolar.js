c462d0e5b6cbba1ec0fc50e298348267fa9a63d8
Patricio Pitaluga <patricio.pitaluga@avatarla.com>
2018-09-24 11:24:12 -0300

    Fix

--- bin/dolar.js
+++ bin/dolar.js
@@ -5,8 +5,8 @@ const axios = require('axios');
 console.log('Getting current value...');
 axios.get('https://www.google.com.ar/search?q=1usd+to+ars&oq=1usd+to+ars&aqs=chrome..69i57.5664j0j1&sourceid=chrome&ie=UTF-8')
   .then(function (response) {
-    let valDolar = response.data.substr(response.data.indexOf('<b>1 USD</b> = ') + 15);
-    valDolar = valDolar.substr(0, valDolar.indexOf(' '));
+    let valDolar = response.data.substr(response.data.indexOf('knowledge-currency__tgt-amount">') + 32);
+    valDolar = valDolar.substr(0, valDolar.indexOf('<'));
     console.log('1usd = $' + valDolar + ' pesos argentinos.');
   })
   .catch(function (error) {
